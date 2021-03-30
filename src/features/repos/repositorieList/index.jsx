38965eaad408b58888c14976f3d95455d36afca9
Jerson Q <solorsanolopez@gmail.com>
2021-03-29 21:40:36 -0700

    [Add] SearchBar and useSearchItems custom hook

--- src/features/repos/repositorieList/index.jsx
+++ src/features/repos/repositorieList/index.jsx
@@ -1,25 +1,24 @@
 import React from 'react';
+import SearchBar from '../../UI/searchBar';
 import RepositorieListItem from '../repositorieListItem';
+import useSearchItems from '../../../hooks/useSearchItems';
 
 const RepositorieList = ({ repos }) => {
+  const { query, setQuery, filteredItems } = useSearchItems(repos);
+
   return (
     <div className="box">
       <nav className="panel">
         <p className="panel-heading">Repositories</p>
       </nav>
       <div className="panel-block">
-        <p className="control has-icons-left">
-          <input className="input" type="text" placeholder="Search" />
-          <span className="icon is-left">
-            <i className="fas fa-search" aria-hidden="true"></i>
-          </span>
-        </p>
+        <SearchBar query={query} setQuery={setQuery} />
       </div>
       <p className="panel-tabs">
         <span className="is-active">All</span>
       </p>
       <ul>
-        {repos.map((repo) => {
+        {filteredItems.map((repo) => {
           return (
             <li key={repo.id}>
               <RepositorieListItem repo={repo} />
