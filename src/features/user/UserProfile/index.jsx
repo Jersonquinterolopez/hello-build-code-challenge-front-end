f8df1727c89683855fa4e41fd4160aafa2621e18
Jerson Q <solorsanolopez@gmail.com>
2021-04-01 11:35:19 -0700

    [Add] CalendarEntries component to UserProfile

--- src/features/user/UserProfile/index.jsx
+++ src/features/user/UserProfile/index.jsx
@@ -1,17 +1,25 @@
 import React from 'react';
 import { useDispatch, useStore } from '../../../store/StoreProvider';
 import AccessInfo from '../AccessInfo';
+import CalendarEntries from '../CalendarEntries';
 
 const UserProfile = () => {
-  const { user } = useStore();
+  const { user, upcomingEvents } = useStore();
   const dispatch = useDispatch();
 
   return (
-    <div>
-      <AccessInfo user={user} dispatch={dispatch} />
-      {/* <CalendarEntries upcomingEvents={upcomingEvents} />
-      // list favorites and send it to the database
-      <FavoriteRepos favoriteRepos={favoriteRepos} /> */}
+    <div className="section">
+      <div className="columns">
+        <div className="column">
+          <AccessInfo user={user} dispatch={dispatch} />
+        </div>
+        <div className="column">
+          <CalendarEntries upcomingEvents={upcomingEvents} />
+        </div>
+      </div>
+
+      {/* list favorites and send it to the database */}
+      {/* <FavoriteRepos favoriteRepos={favoriteRepos} /> */}
     </div>
   );
 };
